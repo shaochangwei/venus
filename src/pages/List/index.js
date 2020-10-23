6642d9a4df3d4d089f91d6515e3ecaf7f6a41662
shaochangwei <1425273970@qq.com>
2020-10-23 22:21:10 +0800

    add

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -9,13 +9,26 @@ import './index.less';
 @inject('actList')
 @observer
 export default class List extends Component {
-  
+  state={
+    choose:0,
+    show:false,
+    list:[123,123,123,123,123,123,123,123,123,123,123]
+  }
   componentDidMount(){
    this.props.actList.getList()
   }
+  hanleShow=()=>{
+    this.setState({
+      show:!this.state.show
+    })
+  }
+  onChoose=(index)=>{
+    this.setState({
+      choose:index
+    })
+  }
   render() {
     let datalist = this.props.actList.dataList.toJS()
-    console.log(datalist)
     return (
       <div>
         <div>
@@ -37,6 +50,22 @@ export default class List extends Component {
           </Route>
         </div>
         <Button type="primary">这是列表啊</Button>
+        <div className="exam" style={{height:this.state.show?"100px":""}}>
+        <div className="left">
+          <div className="flex">
+            
+            {
+              this.state.list.map((item,index)=>{
+              return <div className={`flex-item ${this.state.choose==index?"active":""}`} onClick={()=>{this.onChoose(index)}}>{item}</div>
+              })
+            }
+          </div>
+          
+          </div>
+          <div className="right" onClick={this.hanleShow}>
+            jaskldjklsja
+          </div>
+          </div>
       </div> 
     );
   }
